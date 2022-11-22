50d8b1728b5f949f242cad9b40df4dd0b1d6d216
Daniel Taherkhanian <daniel.taherkhanian@elev.ga.ntig.se>
2022-11-22 11:08:57 +0100

    V0.2 [Added some new lines of code]

--- AdventureRPG/AdventureRPG/Program.cs
+++ AdventureRPG/AdventureRPG/Program.cs
@@ -1,6 +1,6 @@
 ﻿//The Game begins here!
-string PlayAgain = "y";
-while (PlayAgain == "y")
+string playAgain = "y";
+while (playAgain == "y")
 {
     string name;
 
@@ -14,7 +14,26 @@ while (PlayAgain == "y")
     int enemyMonsterHealth = 150;
 
     Random generator = new Random();
+
+    Console.WriteLine("You wake up in a dark room. The tattered curtains are closed, sunlight peeking through the multitude of holes dotting the biege fabric. You turn your head to see what your alarm clock says. It reads: '06:47'. Do you get out of bed? \n\n [Yes // No]");
     
+    string wakeUp = "";
+    while (wakeUp != "yes" && wakeUp != "no")
+    {
+        wakeUp = Console.ReadLine();
+        if (wakeUp != "yes" || wakeUp != "no")
+        {
+            Console.WriteLine("Please write yes or no!!!!"); //gnäll
+        }
+    }
+
+
+
+    if (wakeUp == "Yes")
+    {
+        Console.WriteLine();
+        wakeUp =
+    }
 }
 Console.ReadLine();
 
