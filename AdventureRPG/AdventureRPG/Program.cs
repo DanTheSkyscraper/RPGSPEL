9ddb2677ae855966aff1076dc2544dcd670b96ce
Daniel Taherkhanian <daniel.taherkhanian@elev.ga.ntig.se>
2022-11-22 10:07:48 +0100

    first commit

---
+++ AdventureRPG/AdventureRPG/Program.cs
@@ -0,0 +1,25 @@
+﻿//The Game begins here!
+string PlayAgain = "y";
+while (PlayAgain == "y")
+{
+    string name;
+
+    int unarmedMinDamage = 0;
+    int unarmedMaxDamage = 10;
+    int weaponMinDamage = 0;
+    int weaponMaxDamage = 20;
+
+    int playerHealth = 100;
+    int enemyHumanHealth = 100;
+    int enemyMonsterHealth = 150;
+
+    Random generator = new Random();
+    
+}
+Console.ReadLine();
+
+
+
+
+
+//Console.WriteLine("\n\n\n\n"); Det här är hur man gör mellanrum
